ca69a3b177ca280b9b9d0fd4baa4d16d8196afc6
Mohamed Amine Amara <mlamara@mobelite.fr>
2017-08-28 17:20:38 +0100

    ws connct

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Slides, ToastController } from 'ionic-angular';
+
+import * as WC from 'woocommerce-api';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +9,74 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  WooCommerce: any;
+  products: any[];
+  moreProducts: any[];
+  page:number;
+
+  @ViewChild('productSlides') productSlides: Slides;
+
+  constructor(public navCtrl: NavController, public toastCtrl:ToastController) {
+
+    this.page =2;
+    this.WooCommerce = WC({
+      url: "http://afternoon-ravine-20342.herokuapp.com",
+      consumerKey: "ck_b11071b5f55c591fb96e5aa743dbed35904eb762",
+      consumerSecret: "cs_8d427f0177b797b333bbbd803ddebfb9beb21b41",
+      wpAPI: true,
+      version: 'wc/v2'
+    });
+
+    this.WooCommerce.getAsync("products").then((data) => {
+      console.log(JSON.parse(data.body));
+      this.products = JSON.parse(data.body);
+    }, (err) => {
+      console.log(err);
+    });
+
+    this.loadMoreProducts(null);
+
+  }
+
+  ionViewDidLoad(){
+    setInterval(()=>{
+      if(this.productSlides.getActiveIndex() == this.productSlides.length() -1){
+        this.productSlides.slideTo(0);
+      }
+      this.productSlides.slideNext();
+    },3000);
+  }
+
+  loadMoreProducts(event){
+    if(event == null)
+    {
+      this.page = 2;
+      this.moreProducts = [];
+    }else{
+      this.page++;
+    }
+
+    this.WooCommerce.getAsync("products?page="+this.page).then((data) => {
+      console.log(JSON.parse(data.body));
+      this.moreProducts = this.moreProducts.concat(JSON.parse(data.body));
+      if(event != null)
+      {
+        event.complete();
+      }
+
+      if(JSON.parse(data.body).length < 10){
+        event.enable(false);
+
+        this.toastCtrl.create({
+          message: "No more products !",
+          duration: 5000
+        }).present();
+      }
+
+
+    }, (err) => {
+      console.log(err);
+    });
 
   }
 
--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {NavController, NavParams } from 'ionic-angular';
 import {HomePage} from '../home/home';
 
+import * as WC from 'woocommerce-api';
+
 /**
  * Generated class for the MenuPage page.
  *
@@ -15,9 +17,37 @@ import {HomePage} from '../home/home';
 })
 export class MenuPage {
   homePage:Component;
+  WooCommerce: any;
+  categories: any[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.homePage = HomePage;
+
+    this.categories = [];
+
+    this.WooCommerce = WC({
+      url: "http://afternoon-ravine-20342.herokuapp.com",
+      consumerKey: "ck_b11071b5f55c591fb96e5aa743dbed35904eb762",
+      consumerSecret: "cs_8d427f0177b797b333bbbd803ddebfb9beb21b41",
+      wpAPI: true,
+      version: 'wc/v2'
+    });
+
+
+    this.WooCommerce.getAsync("products/categories").then((data) => {
+      console.log(JSON.parse(data.body));
+      let temp:any[] = JSON.parse(data.body);
+
+      for(let i =0;i<temp.length; i++){
+        if(temp[i].parent == 0){
+          this.categories.push(temp[i]);
+          console.log(this.categories)
+        }
+      }
+    }, (err) => {
+      console.log(err);
+    });
+
   }
 
   ionViewDidLoad() {
